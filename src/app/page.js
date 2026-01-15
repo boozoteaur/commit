e9b059ba0a9002685eb6e30411f835c9034bf891
LopatinEugene <lopatineugene@outlook.com>
2026-01-15 16:13:04 +0900

    initial

--- src/app/page.js
+++ src/app/page.js
@@ -4,37 +4,31 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
+       <main className={styles.main}>
+        <div className={styles.top}>
+          <div className={styles.topinfo}>
+            <Image
+            className={styles.logo}
+            src="/logo.jpg"
+            alt="commit-it logo"
+            width={150}
+            height={150}
+            priority
+            />
+            <div className={styles.phone}>+7 914 273-90-22</div>
+            <div className={styles.mail}>commit23@mail.ru</div>
+            </div>
+            <div className={styles.menubox}>
+              <div className={styles.menu}>
+                <div className={styles.menuitem}>О компании</div>
+                <div className={styles.menuitem}>Портфолио</div>
+                <div className={styles.menuitem}>Контакты</div>
+              </div>
+            </div>
+          
+          
+        </div>  
+        
         <div className={styles.ctas}>
           <a
             className={styles.primary}
